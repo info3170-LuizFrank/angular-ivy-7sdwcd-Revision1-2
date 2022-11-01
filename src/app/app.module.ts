1da056fbc7a57448e9e8d62dbd131218c8538092
info3170-LuizFrank <113394681+info3170-LuizFrank@users.noreply.github.com>
2022-11-01 17:17:59 -0400

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,12 @@ import { RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatBadgeModule } from '@angular/material/badge';
+
 @NgModule({
   imports: [
     BrowserModule,
@@ -25,13 +31,18 @@ import { AboutusComponent } from './aboutus/aboutus.component';
       { path: '**', component: PageNotFoundComponent },
     ]),
     FormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    BrowserAnimationsModule,
+    MatBadgeModule,
   ],
   declarations: [
     AppComponent,
     ProductsListComponent,
     ShoppingCartComponent,
     PageNotFoundComponent,
-    AboutusComponent,
+    AboutusComponent
   ],
   bootstrap: [AppComponent],
 })
